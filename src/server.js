9d234e77b931e574df5041a13ff567c83b0331ba
sergiogt93 <sergio.ber0556@gmail.com>
2022-05-15 16:27:27 +0200

    cors enabled

--- src/server.js
+++ src/server.js
@@ -7,14 +7,6 @@ server.use(cors());
 server.use(express.json());
 server.disable("x-powered-by");
 
-server.use(function (req, res) {
-  const origin = req.header("Origin");
-
-  if (trustedOrigins.indexOf(origin) >= 0) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-});
-
 server.use(morgan("dev"));
 
 module.exports = server;
